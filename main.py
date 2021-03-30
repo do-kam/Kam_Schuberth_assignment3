7f0a3baeeb8a492546085dfb961ce2438bc90ad5
do-kam <dorothea.kam@gmail.com>
2021-03-30 16:54:11 +0200

    init commit

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ pd.set_option("display.precision", 12)
 daten2 = pd.read_csv('input_form.csv',delimiter=";", header = None)
 print(daten2)
 
-import matplotlib.pyplot as plt
-plt.figure(0)
-plt.scatter (daten2, daten2)
+#import matplotlib.pyplot as plt
+#plt.figure(0)
+#plt.scatter(daten2, daten2)
 
 
 #funktion um distance zu berechnen
