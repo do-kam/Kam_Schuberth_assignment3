47a9e24282e8b2ab91886651ca0490444bed955f
do-kam <dorothea.kam@gmail.com>
2021-03-30 18:05:20 +0200

    init commit

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ print(daten2)
 #plt.figure(0)
 #plt.scatter(daten2, daten2)
 
+#aufrufen der funktion init to initialize object from class
+#source: https://github.com/python-engineer/MLfromscratch/tree/master/mlfromscratch
+class kmeansalgorithm():
+
+    def __init__(self, K=3, max_iters=100, plot_steps=False):
+        self.K = K
+        self.max_iters = max_iters
+        self.plot_steps = plot_steps
+
+        # list of sample indices for each cluster
+        self.clusters = [[] for _ in range(self.K)]
+        # the centers (mean feature vector) for each cluster: empty at the beginning
+        self.centroids = []
+
 
 #funktion um distance zu berechnen
 def manhattan_distance(x1,x2):
